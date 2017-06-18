98a311ae2c918e79177127d4d67f252ec478036f
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-17 21:31:46 -0400

    updated css styles + html box + js DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 /*aquí va tu código*/
+
+//Llamar el navegador
+window.addEventListener("load", begin, false);
+
+function begin() {
+    var button = document.getElementById("record");//Identificar el boton
+    button.addEventListener("click", newItem, false);//crea un nuevo item con los valores del formulario
+    hide(document.getElementById("comment-appear")); //le agrego la clase hide a la caja
+}
+function newItem() { //guardar nombre y comentario
+    var name = document.getElementById("name").value;
+    var comment = document.getElementById("comment").value;
+    sessionStorage.setItem(name, comment);//guardar informaciòn; busca los elementos; GUARDAME LA INFORMACION
+    //Crea un nuevo item con la informaciòn que se ha guardado
+    showInformation(name);
+}
+function showInformation(name) {//lee la informacion guardada, utilizando la api web storage
+    var showData = document.getElementById("comment-appear");//donde quiero que aparezcan mis datos;
+    var storage = sessionStorage.getItem(name); //rescatar info almacenada que se ha almacenado
+
+    var text = " Nombre: " + name + '\n' + "Comentario:" + '\n' +storage;
+
+    var create_div = document.createElement('DIV'); //creo el div que me va a contener la informacion
+    var textnode = document.createTextNode(text); // creo el nodo del texto
+    create_div.appendChild(textnode); //creo un hijo del div q sera textnote
+    showData.appendChild(create_div); //creo un hijo de la caja principal que me va a contener el div
+    show(showData);
+}
+
+
+function hide(element) { //Esconder elementos
+    element.classList.add('hide');
+}
+
+function show(element) { //Mostrar elementos
+    element.classList.remove('hide');
+}
+
+//SET ITEM: Crear item
+//SESSION STORAGE:
+
+//CREATE BY LAUREENPE
